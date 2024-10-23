d8683ce6e4c13dcf0e07346768fa90accce28154
kiang <kiange@gmail.com>
2024-10-23 20:04:12 +0800

    write the content to another time based file and added to list

--- scripts/01_fetch_genary.php
+++ scripts/01_fetch_genary.php
@@ -16,4 +16,22 @@ $text = exec("curl 'https://www.taipower.com.tw/d006/loadGraph/loadGraph/data/ge
   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
   -H 'x-requested-with: XMLHttpRequest'");
 
-file_put_contents($basePath . '/docs/genary.json', json_encode(json_decode($text), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+$json = json_decode($text, true);
+$t = strtotime($json[key($json)] . ':00');
+
+file_put_contents($basePath . '/docs/genary.json', json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+$targetPath = $basePath . '/docs/genary/' . date('Y/Ymd', $t);
+if (!file_exists($targetPath)) {
+  mkdir($targetPath, 0755, true);
+}
+file_put_contents($targetPath . '/' . date('His', $t) . '.json', json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+$listFile = $targetPath . '/list.json';
+if (file_exists($listFile)) {
+  $list = json_decode(file_get_contents($listFile), true);
+} else {
+  $list = [];
+}
+$list[] = date('His', $t);
+sort($list);
+file_put_contents($listFile, json_encode($list, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
